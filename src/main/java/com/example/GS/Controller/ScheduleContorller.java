3309b33d83a0b9446aa54b4630bb73b4ec367446
krupankan <krupankan@ivoyant.com>
2023-06-26 12:43:03 +0530

    first commit

--- src/main/java/com/example/GS/Controller/ScheduleContorller.java
+++ src/main/java/com/example/GS/Controller/ScheduleContorller.java
@@ -1,18 +1,25 @@
 package com.example.GS.Controller;
 
-import org.apache.tomcat.jni.Sockaddr;
+import org.Ivoyant.model.Schedule;
+import org.Ivoyant.service.ScheduleImpl;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import java.time.LocalDateTime;
-import java.util.Optional;
+import java.sql.SQLException;
 
 @RestController
 public class ScheduleContorller {
+    ScheduleImpl schedule = new ScheduleImpl("jdbc:postgresql://localhost:5432/testingdb","nkharvi","123");
 
-    @GetMapping("/welcome")
-    public ResponseEntity schedule() {
-        System.out.println("The service has been invoked at this time , "+ LocalDateTime.now());
-         return ResponseEntity.ok("welcome");
+    @GetMapping("/getScheduleById/{id}")
+    public ResponseEntity schedule(@PathVariable int id) throws SQLException {
+        Schedule schedule1 = schedule.getSchedule(id);
+        if(schedule1 == null)
+            return ResponseEntity.status(404).body("Not found");
+        return ResponseEntity.ok(schedule1);
+    }
+    @PostMapping("/createSchedule")
+    public  ResponseEntity create(@RequestBody Schedule cschedule) throws SQLException {
+        Schedule schedule1 = schedule.create(cschedule);
+        return ResponseEntity.status(201).body("created");
     }
 }
