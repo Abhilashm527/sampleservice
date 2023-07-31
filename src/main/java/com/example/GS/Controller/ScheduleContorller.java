e3a8a162b9735a4cfefe646efb55163e248a98dc
krupankan <krupankan@ivoyant.com>
2023-07-31 10:37:27 +0530

    cdjs

--- src/main/java/com/example/GS/Controller/ScheduleContorller.java
+++ src/main/java/com/example/GS/Controller/ScheduleContorller.java
@@ -8,18 +8,9 @@ import java.sql.SQLException;
 
 @RestController
 public class ScheduleContorller {
-    ScheduleImpl schedule = new ScheduleImpl("jdbc:postgresql://localhost:5432/testingdb","nkharvi","123");
 
-    @GetMapping("/getScheduleById/{id}")
-    public ResponseEntity schedule(@PathVariable int id) throws SQLException {
-        Schedule schedule1 = schedule.getSchedule(id);
-        if(schedule1 == null)
-            return ResponseEntity.status(404).body("Not found");
-        return ResponseEntity.ok(schedule1);
-    }
-    @PostMapping("/createSchedule")
-    public  ResponseEntity create(@RequestBody Schedule cschedule) throws SQLException {
-        Schedule schedule1 = schedule.create(cschedule);
-        return ResponseEntity.status(201).body("created");
+    @GetMapping("/helloworld")
+    public ResponseEntity schedule() {
+    return ResponseEntity.ok("Hello world");
     }
 }
