000d7e3a2203b0db612fc02c906f0265d590ab56
krupankan <krupankan@ivoyant.com>
2023-07-31 10:42:10 +0530

    cdjs

--- src/main/java/com/example/GS/Controller/ScheduleContorller.java
+++ src/main/java/com/example/GS/Controller/ScheduleContorller.java
@@ -1,10 +1,8 @@
 package com.example.GS.Controller;
 
-import org.Ivoyant.model.Schedule;
-import org.Ivoyant.service.ScheduleImpl;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import java.sql.SQLException;
+
 
 @RestController
 public class ScheduleContorller {
